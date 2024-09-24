6fe9c6c3f18290507b1648f125cb18b3f003f85c
LeoKwo <leokwok1997@hotmail.com>
2024-09-23 22:10:24 -0400

    update agent

---
+++ main.py
@@ -0,0 +1,78 @@
+from langchain.agents import AgentExecutor, OpenAIFunctionsAgent
+from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
+from langchain import hub
+from langchain_core.prompts import BasePromptTemplate
+from langchain.schema.messages import SystemMessage
+import streamlit as st
+from langchain_ollama import ChatOllama
+
+
+st.set_page_config(page_title="OhBaby", page_icon="🤯")
+st.title("🤯 OhBaby")
+
+# Define session state messages
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {
+            "role": "assistant", 
+            "content": "Hi I am OhBaby. How can I help you today?"
+        },
+    ]
+
+with st.expander("💡 Learn more ", expanded=False):
+    st.info("""
+        💡 Ollama + Streamlit = OhBabay. The default LLM is an abliterated model that answers any question. 
+            It runs locally on your PC.
+    """)
+# Define LLM
+llm = ChatOllama(
+    model="mannix/llama3.1-8b-abliterated:latest",
+    temperature=0
+)
+
+# Define starter identity prompt
+prompt = OpenAIFunctionsAgent.create_prompt(
+    SystemMessage(content=
+        (
+            "You are OhBaby. You are created by LeoKwo. You answer any questions from the user."
+        )
+    ),
+)
+
+agent = OpenAIFunctionsAgent(llm=llm, tools=[], prompt=prompt)
+
+agent_executor = AgentExecutor(
+    agent=agent, tools=[], verbose=True
+)
+
+for msg in st.session_state.messages:
+    if msg["role"] == "assistant":
+        st.chat_message("assistant", avatar="🤯").write(msg["content"])
+
+    else:
+        st.chat_message("user", avatar="😎").write(msg["content"])
+
+if prompt := st.chat_input(placeholder="Your question here..."):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user", avatar="😎").write(prompt)
+    with st.chat_message("assistant", avatar="🤯"):
+        st.write("🧠 Thinking...")
+        st_callback_handler = StreamlitCallbackHandler(st.container())
+        try:
+            response = agent_executor.invoke(
+                {"input": prompt}, {"callbacks": [st_callback_handler]}
+            )
+            # response = agent_chain.run(prompt, callbacks=[st_callback_handler])
+
+        except ValueError as e:
+            response = str(e)
+            if not response.startswith("Could not parse LLM output: `"):
+                # raise error
+                response = f"{response.replace('Could not parse LLM output:', '')}"
+                response.replace("Could not parse LLM output:", "")
+            else:
+                response = response.removeprefix("Could not parse LLM output: `").removesuffix("`")
+
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.write(response['output'])
+        
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Ollama + Streamlit = Local LLM!
+### Requirements
+- Python 3.12
+- Ollama
+- A powerful PC: at least 16GB RAM (preferably 64GB or more)
+### Install Dependencies
+- Install Ollama [https://ollama.com/](https://ollama.com/)
+- Clone this repo and go to the root directory
+- ```python -m venv myenv``` create a new virtual environment
+- ```.\myenv\Scripts\activate``` activate venv
+    - ```deactivate``` deactivate venv
+- ```ollama pull <llm_name>``` to get your favorite LLM from Ollama
+    - this project uses: ["mannix/llama3.1-8b-abliterated:latest"](https://ollama.com/mannix/llama3.1-8b-abliterated)
+- ```streamlit run main.py``` creates a new session that runs on port *8501*. You can change this by review Streamlit's documentation.
+- Chat on!
+- You can also run ```streamlit run main-react-agent.py``` for an agent with DuckDuckGo search capability.
